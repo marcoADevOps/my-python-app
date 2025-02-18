d51a36a915148b17167cd3859d2d2f76a50526ea
marcoADevOps <m.aaurelio.lima.silva@gmail.com>
2025-02-18 02:02:25 +0000

    Updated application with new changes

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello, GitOps! This is my Python app running on Kubernetes.Now you are seeing the version 2 of this application"
+    return "Hello, GitOps! This is my Python app running on Kubernetes."
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
